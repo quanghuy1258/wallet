bc6bb9013feefab4222324f2f2454af942fe8c9f
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2019-11-11 01:36:36 +0700

    Update code

--- wallet.h
+++ wallet.h
@@ -1,11 +1,31 @@
 #ifndef WALLET_H
 #define WALLET_H
 
+#include <string>
 
-class Wallet
-{
+class Wallet {
 public:
-    Wallet();
+  Wallet();
+  ~Wallet();
+
+  // Encrypt wallet.
+  bool encryptWallet(const std::string &wallet_passphrase);
+
+  // Return whether wallet is encrypted.
+  bool isCrypted();
+
+  // Lock wallet.
+  bool lock();
+
+  // Unlock wallet.
+  bool unlock(const std::string &wallet_passphrase);
+
+  // Return whether wallet is locked.
+  bool isLocked();
+
+  // Change wallet passphrase.
+  bool changeWalletPassphrase(const std::string &old_wallet_passphrase,
+                              const std::string &new_wallet_passphrase);
 };
 
 #endif // WALLET_H
