fde0152136528a0cc4d6edb28ef56b2fb6d247bb
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2020-03-19 17:50:28 +0700

    Update db: not completely

--- berkeley_db.cpp
+++ berkeley_db.cpp
@@ -1,5 +1,25 @@
 #include "berkeley_db.h"
+#include "util.h"
 
-BerkeleyEnvironment::BerkeleyEnvironment(const std::string &env_directory) {
+BerkeleyEnvironment::BerkeleyEnvironment(const QDir &env_directory) {
   _path = env_directory;
+  reset();
 }
+
+BerkeleyEnvironment::~BerkeleyEnvironment() { close(); }
+
+void BerkeleyEnvironment::reset() {
+  dbEnv.reset(new DbEnv(DB_CXX_NO_EXCEPTIONS));
+  _fDbEnvInit = false;
+}
+
+bool BerkeleyEnvironment::isInitialized() const { return _fDbEnvInit; }
+
+bool BerkeleyEnvironment::isDatabaseLoaded(
+    const std::string &dbFilename) const {
+  return mapDatabases.count(dbFilename);
+}
+
+QDir BerkeleyEnvironment::getDirectory() const { return _path; }
+
+void BerkeleyEnvironment::close() {}
--- berkeley_db.h
+++ berkeley_db.h
@@ -1,21 +1,41 @@
 #ifndef BERKELEY_DB_H
 #define BERKELEY_DB_H
 
+#include <condition_variable>
+#include <functional>
+#include <map>
+#include <memory>
 #include <string>
+
+#include <QDir>
 
 #define HAVE_CXX_STDHEADERS
 #include <db_cxx.h>
 
+class BerkeleyEnvironment;
+class BerkeleyDatabase;
+class BerkeleyBatch;
+
 class BerkeleyEnvironment {
 private:
   bool _fDbEnvInit;
-  std::string _path;
+  QDir _path;
 
 public:
-  BerkeleyEnvironment(const std::string &env_directory);
+  std::unique_ptr<DbEnv> dbEnv;
+  std::map<std::string, int> mapFileUseCount;
+  std::map<std::string, std::reference_wrapper<BerkeleyDatabase>> mapDatabases;
+  std::condition_variable cvDbInUse;
+
+  BerkeleyEnvironment(const QDir &env_directory);
+  ~BerkeleyEnvironment();
   void reset();
 
-  void reloadDbEnv();
+  bool isInitialized() const;
+  bool isDatabaseLoaded(const std::string &dbFilename) const;
+  QDir getDirectory() const;
+
+  void close();
 };
 
 class BerkeleyDatabase {};
